9a0cedd8fbdc27b81758f1e885b7830270e7d15f
Jakob Staudt <staudt.jakob@web.de>
2019-07-04 21:16:03 +0200

    Add method to draw dots that appear when laser is on and doesnt move

--- laserPlotter.py
+++ laserPlotter.py
@@ -5,6 +5,9 @@ import math
 
 drawInstantly = False
 drawTravel = True
+drawWaitBurn = True
+maxBurnDia = 2
+burnTimeCoefficient = 800
 cutColor = "black"
 travelColor = "red"
 
@@ -23,6 +26,7 @@ if drawInstantly:
     turtle.tracer(0, 0)
 
 turtle.penup()
+laserEnabled = False
 turtle.speed(0)
 turtle.goto(xOffset, yOffset)
 
@@ -31,7 +35,7 @@ with open(fileName) as file:
 
 for line in fileList:
     line = line.strip("\n")
-    if line[:2] == "G0" or line[:2] == "G1":
+    if line.startswith("G0") or line.startswith("G1"):
         xPos = re.search("X(\d+(\.\d+)?)", line)
         if xPos != None:
             xPos = float(xPos[0][1:])
@@ -40,11 +44,19 @@ for line in fileList:
             yPos = float(yPos[0][1:])
         if xPos != None and yPos != None:
             turtle.goto(xPos * canvasScale + xOffset, yPos * canvasScale + yOffset)
+    elif line.startswith("G4") and laserEnabled and drawWaitBurn:
+        waitTime = re.search("P\d+", line)
+        if waitTime != None:
+            waitTime = int(waitTime[0][1:])
+            pointDia = maxBurnDia - (maxBurnDia * (math.e ** (waitTime / -burnTimeCoefficient)))
+            turtle.dot(pointDia * canvasScale)
     elif line.startswith(laserEnable):
+        laserEnabled = True
         if drawTravel:
             turtle.color("black")
         turtle.pendown()
     elif line.startswith(laserDisable):
+        laserEnabled = False
         if drawTravel:
             turtle.color("red")
         else:
